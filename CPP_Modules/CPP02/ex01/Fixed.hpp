dd81087d3e0b17cda41d576ae375f295634603ad
Zeni <147077236+Zen-ny56@users.noreply.github.com>
2024-06-29 13:12:05 +0000

    00

--- CPP_Modules/CPP02/ex01/Fixed.hpp
+++ CPP_Modules/CPP02/ex01/Fixed.hpp
@@ -1,6 +1,7 @@
 #pragma once 
 
 #include <iostream>
+#include <cmath>
 
 class Fixed
 {
@@ -9,8 +10,14 @@ class Fixed
 		static const int fractional;
 	public:
 		Fixed();
+		Fixed(const int num);
+		Fixed(const float f);
 		Fixed& operator=(const Fixed& copy);
 		~Fixed();
 		int	getRawBits(void) const;
 		void setRawBit(int const raw);
-}
+		float toFloat(void) const;
+		int toInt(void) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
