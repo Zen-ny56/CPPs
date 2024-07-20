0d3f57c6097f02cb105e58f0a47915c7346f7491
Zeni <147077236+Zen-ny56@users.noreply.github.com>
2024-07-20 12:50:20 +0000

    34

--- CPP_Modules/CPP04/ex01/main.cpp
+++ CPP_Modules/CPP04/ex01/main.cpp
@@ -5,29 +5,29 @@
 
 int main() {
    
-    // std::cout << "Test 4: Deep Copy" << std::endl;
-    // Dog originalDog;
-    // originalDog.setBrainIdea(0, "Chase the cat");
-    // Dog copiedDog = originalDog;
-    // std::cout << "Original Dog's Idea: " << originalDog.getBrainIdea(0) << std::endl;
-    // std::cout << "Copied Dog's Idea: " << copiedDog.getBrainIdea(0) << std::endl;
+    std::cout << "Test 4: Deep Copy" << std::endl;
+    Dog originalDog;
+    originalDog.setBrainIdea(0, "Chase the cat");
+    Dog copiedDog = originalDog;
+    std::cout << "Original Dog's Idea: " << originalDog.getBrainIdea(0) << std::endl;
+    std::cout << "Copied Dog's Idea: " << copiedDog.getBrainIdea(0) << std::endl;
     
-    // copiedDog.setBrainIdea(0, "Sleep");
-    // std::cout << "After changing copied dog:" << std::endl;
-    // std::cout << "Original Dog's Idea: " << originalDog.getBrainIdea(0) << std::endl;
-    // std::cout << "Copied Dog's Idea: " << copiedDog.getBrainIdea(0) << std::endl;
+    copiedDog.setBrainIdea(0, "Sleep");
+    std::cout << "After changing copied dog:" << std::endl;
+    std::cout << "Original Dog's Idea: " << originalDog.getBrainIdea(0) << std::endl;
+    std::cout << "Copied Dog's Idea: " << copiedDog.getBrainIdea(0) << std::endl;
 
-    // Cat originalCat;
-    // originalCat.setBrainIdea(0, "Climb the tree");
-    // Cat copiedCat = originalCat;
-    // std::cout << "Original Cat's Idea: " << originalCat.getBrainIdea(0) << std::endl;
-    // std::cout << "Copied Cat's Idea: " << copiedCat.getBrainIdea(0) << std::endl;
+    Cat originalCat;
+    originalCat.setBrainIdea(0, "Climb the tree");
+    Cat copiedCat = originalCat;
+    std::cout << "Original Cat's Idea: " << originalCat.getBrainIdea(0) << std::endl;
+    std::cout << "Copied Cat's Idea: " << copiedCat.getBrainIdea(0) << std::endl;
     
-    // copiedCat.setBrainIdea(0, "Nap");
-    // std::cout << "After changing copied cat:" << std::endl;
-    // std::cout << "Original Cat's Idea: " << originalCat.getBrainIdea(0) << std::endl;
-    // std::cout << "Copied Cat's Idea: " << copiedCat.getBrainIdea(0) << std::endl;
-    // return 0;
+    copiedCat.setBrainIdea(0, "Nap");
+    std::cout << "After changing copied cat:" << std::endl;
+    std::cout << "Original Cat's Idea: " << originalCat.getBrainIdea(0) << std::endl;
+    std::cout << "Copied Cat's Idea: " << copiedCat.getBrainIdea(0) << std::endl;
+    return 0;
     // std::cout << "Test 5: Assignment Operator" << std::endl;
     // Dog dog1;
     // dog1.setBrainIdea(0, "Fetch the ball");
