8423c845ce54c04ce64eca705e70e9f699f04aa2
Zeni <147077236+Zen-ny56@users.noreply.github.com>
2024-10-09 15:10:31 +0000

    1

--- CPP_Modules/CPP08/ex02/MutantStack.hpp
+++ CPP_Modules/CPP08/ex02/MutantStack.hpp
@@ -4,12 +4,11 @@
 #include <iostream>
 
 template <typename T>
-class MutantStack : public std::stack<T>
+class MutantStack : public std::stack<T> // Not a true container but an adapter that wraps around internal constainers such as <std::deque> , or <std::vector>
 {
 	public:
-		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::iterator iterator; //Basically grabbing the iterator that belongs to the hidden container 
 		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-
 	MutantStack();
 	MutantStack(const MutantStack& other);
 	~MutantStack();
